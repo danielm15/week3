c42354d3e2563e5fc86d055977a89b771554fac0
Daniel <danielm15@ru.is>
2017-12-14 04:25:01 +0000

    Changed jenkins create and delete instance scripts to use the scripts from create and delete docker instance scripts

--- provisioning/cicd/create-jenkins-instance.sh
+++ provisioning/cicd/create-jenkins-instance.sh
@@ -1,37 +1,65 @@
-#!/usr/bin/env bash
+#!/bin/bash
+set -e
 
-THISDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+echo "Check for instance information..."
+INSTANCE_DIR=~/aws
 
-. ${THISDIR}/../ec2/functions.sh
+export AMI_IMAGE_ID="ami-e7d6c983"
 
-[ -d ~/aws ] || mkdir ~/aws
+echo No instance information present, continuing.
+[ -d ${INSTANCE_DIR} ] || mkdir ${INSTANCE_DIR}
 
 USERNAME=$(aws iam get-user --query 'User.UserName' --output text)
 
-PEM_NAME=hgop-${USERNAME}
-JENKINS_SECURITY_GROUP=jenkins-${USERNAME}
+SECURITY_GROUP_NAME=hgop-${USERNAME}
 
+echo "Using security group name ${SECURITY_GROUP_NAME}"
+
+if [ ! -e ~/aws/security-group-name.txt ]; then
+    echo ${SECURITY_GROUP_NAME} > ~/aws/security-group-name.txt
+fi
+
+if [ ! -e ${INSTANCE_DIR}/${SECURITY_GROUP_NAME}.pem ]; then
+    aws ec2 create-key-pair --key-name ${SECURITY_GROUP_NAME} --query 'KeyMaterial' --output text > ${INSTANCE_DIR}/${SECURITY_GROUP_NAME}.pem
+    chmod 400 ${INSTANCE_DIR}/${SECURITY_GROUP_NAME}.pem
+fi
 
 if [ ! -e ~/aws/security-group-id.txt ]; then
-    create-security-group ${JENKINS_SECURITY_GROUP}
+    SECURITY_GROUP_ID=$(aws ec2 create-security-group --group-name ${SECURITY_GROUP_NAME} --description "security group for dev environment in EC2" --query "GroupId"  --output=text)
+    echo ${SECURITY_GROUP_ID} > ~/aws/security-group-id.txt
     echo Created security group ${SECURITY_GROUP_NAME} with ID ${SECURITY_GROUP_ID}
 else
     SECURITY_GROUP_ID=$(cat ~/aws/security-group-id.txt)
-    echo Already have ${SECURITY_GROUP_NAME} with ID ${SECURITY_GROUP_ID}
 fi
 
-
+MY_PUBLIC_IP=$(curl http://checkip.amazonaws.com)
 if [ ! -e ~/aws/instance-id.txt ]; then
-    echo "Creating jenkins instance ami-e7d6c983 hgop2017-students ${THISDIR}/bootstrap-jenkins.sh ${PEM_NAME}"
+    echo Create ec2 instance on security group ${SECURITY_GROUP_ID} ${AMI_IMAGE_ID}
+    INSTANCE_INIT_SCRIPT=bootstrap-jenkins.sh
+    INSTANCE_ID=$(aws ec2 run-instances  --user-data file://${INSTANCE_INIT_SCRIPT} --image-id ${AMI_IMAGE_ID} --security-group-ids ${SECURITY_GROUP_ID} --count 1 --instance-type t2.micro --key-name ${SECURITY_GROUP_NAME} --query 'Instances[0].InstanceId'  --output=text)
+    echo ${INSTANCE_ID} > ~/aws/instance-id.txt
 
-    create-ec2-instance ami-e7d6c983 ${SECURITY_GROUP_ID} ${THISDIR}/bootstrap-jenkins.sh ${PEM_NAME}
-else
-    echo "Instance already exists, nothing to do"
+    echo Waiting for instance to be running
+    echo aws ec2 wait --region eu-west-2 instance-running --instance-ids ${INSTANCE_ID}
+    aws ec2 wait --region eu-west-2 instance-running --instance-ids ${INSTANCE_ID}
+    echo EC2 instance ${INSTANCE_ID} ready and available on ${INSTANCE_PUBLIC_NAME}
 fi
 
-echo "Ignoring failures after this point"
+if [ ! -e ~/aws/instance-public-name.txt ]; then
+    export INSTANCE_PUBLIC_NAME=$(aws ec2 describe-instances --instance-ids ${INSTANCE_ID} --query "Reservations[*].Instances[*].PublicDnsName" --output=text)
+    echo ${INSTANCE_PUBLIC_NAME} > ~/aws/instance-public-name.txt
+fi
 
-authorize-access ${JENKINS_SECURITY_GROUP}
+
+MY_CIDR=${MY_PUBLIC_IP}/32
+
+echo Using CIDR ${MY_CIDR} for access restrictions.
+
+set +e
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${MY_CIDR}
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 80 --cidr ${MY_CIDR}
+aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 8080 --cidr ${MY_CIDR}
+set -e
 
 set +e
 scp -o StrictHostKeyChecking=no -i "~/aws/${PEM_NAME}.pem" ec2-user@$(cat ~/aws/instance-public-name.txt):/var/log/cloud-init-output.log ~/aws/cloud-init-output.log
--- provisioning/cicd/delete-jenkins-instance.sh
+++ provisioning/cicd/delete-jenkins-instance.sh
@@ -1,31 +1,20 @@
 #!/usr/bin/env bash
 
-THISDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+INSTANCE_ID=$(cat ~/aws/instance-id.txt)
+SECURITY_GROUP_ID=$(cat ~/aws/security-group-id.txt)
+SECURITY_GROUP_NAME=$(cat ~/aws/security-group-name.txt)
 
-INSTANCE_ID=$(cat ./ec2_instance/instance-id.txt)
-SECURITY_GROUP_ID=$(cat ./ec2_instance/security-group-id.txt)
-USERNAME=$(aws iam get-user --query 'User.UserName' --output text)
+aws ec2 terminate-instances --instance-ids ${INSTANCE_ID}
 
-. ${THISDIR}/../ec2/functions.sh
+echo Waiting for instance to terminate....
+aws ec2 wait --region eu-west-2 instance-terminated --instance-ids ${INSTANCE_ID}
+echo Instance ${INSTANCE_ID} terminated
 
-if [ -e "./ec2_instance/instance-id.txt" ]; then
-    aws ec2 terminate-instances --instance-ids ${INSTANCE_ID}
+rm ~/aws/instance-id.txt
+rm ~/aws/instance-public-name.txt
 
-    echo Waiting for instance to terminate....
-    aws ec2 wait --region eu-west-2 instance-terminated --instance-ids ${INSTANCE_ID}
-    echo Instance ${INSTANCE_ID} terminated
-
-    rm ./ec2_instance/instance-id.txt
-    rm ./ec2_instance/instance-public-name.txt
-fi
-
-
-PEM_NAME=hgop-${USERNAME}
-JENKINS_SECURITY_GROUP=jenkins-${USERNAME}
-
-if [ ! -e ./ec2_instance/security-group-id.txt ]; then
-    SECURITY_GROUP_ID=$(cat ./ec2_instance/security-group-id.txt)
-else
-    delete-security-group ${JENKINS_SECURITY_GROUP}
-    rm ./ec2_instance/security-group-id.txt
-fi
+#aws ec2 delete-security-group --group-id ${SECURITY_GROUP_ID}
+#
+#aws ec2 delete-key-pair --key-name ${SECURITY_GROUP_NAME}
+#
+#rm  -rf ec2_instance
