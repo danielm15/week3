1b6161519bee65c779a07be1a1ef0bd90f52ab1e
Daniel <danielm15@ru.is>
2017-12-15 06:09:06 +0000

    Removed initial id fetch since it was interfering with later variable

--- provisioning/cicd/create-jenkins-instance.sh
+++ provisioning/cicd/create-jenkins-instance.sh
@@ -11,7 +11,7 @@ echo No instance information present, continuing.
 
 USERNAME=$(aws iam get-user --query 'User.UserName' --output text)
 
-SECURITY_GROUP_NAME=hgop-${USERNAME}
+SECURITY_GROUP_NAME=docker-${USERNAME}
 
 echo "Using security group name ${SECURITY_GROUP_NAME}"
 
